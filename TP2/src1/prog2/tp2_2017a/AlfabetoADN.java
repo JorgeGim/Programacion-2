fd43205a2fbf07693c105ce6f9c22064db2a2d79
Yair <Yair@DESKTOP-UUC9KEO>
2017-06-12 10:10:14 -0300

    no le des bola

--- TP2/src1/prog2/tp2_2017a/AlfabetoADN.java
+++ TP2/src1/prog2/tp2_2017a/AlfabetoADN.java
@@ -3,13 +3,39 @@ package prog2.tp2_2017a;
 public class AlfabetoADN implements Alfabeto<Character> 
 {
 
+	private enum Base{A, C, G, T};
+
 	@Override
 	public int tam() { return 4; }
 
 	@Override
-	public int indice(Character elem) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int indice(Character c)
+	{
+		return Base.valueOf(c.toString()).ordinal();
 	}
-
+	
+	@Override
+	public boolean equals(Object o)
+	{
+		AlfabetoADN a2 = (AlfabetoADN) o;
+		
+		if(a2.tam() != tam())
+			return false;
+		
+		boolean ret = true;
+		for(int i=0; i<tam(); ++i)
+		{
+			ret = ret && this.getCaracter(i).equals(a2.getCaracter(i));
+		}
+		
+		return ret;
+	}
+	
+	private Character getCaracter(int i)
+	{
+		if(i < 0 || i > tam())
+			throw new IllegalArgumentException("Indice fuera de rango");
+		
+		return Base.values()[i].name().charAt(0);
+	}
 }
